9b02ac34e3bda9bbec0b8bb5e1b035c1e7207aba
chacutpon <94590860+chacutpon@users.noreply.github.com>
2024-10-07 21:42:11 +0700

    Add files via upload

--- config/firebase.js
+++ config/firebase.js
@@ -5,14 +5,17 @@ var admin = require("firebase-admin");
 var serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,  
+
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
+  auth_uri: process.env.FIREBASE_AUTH_URI,  
+
   token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,   
+
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
 };
 
 admin.initializeApp({
